f943c01b0275b310088ee9d9c5f105ccb48ff216
“Felix <forsethf@gmail.com>
2022-12-11 12:24:08 +0100

    Final Changes

--- plotting/plot_probabilities.py
+++ plotting/plot_probabilities.py
@@ -8,7 +8,7 @@ noslit = np.loadtxt('plotting/datafiles/probmatrix0.txt')
 doslit = np.loadtxt('plotting/datafiles/probmatrix2.txt')
 t = np.linspace(0, 0.008, len(doslit))
 plt.xlabel('t')
-plt.ylabel('1 - $\sum_{i,j}u^{0*}_{ij}u^{0}_{ij}$')
+plt.ylabel('|1 - $\sum_{i,j}u^{0*}_{ij}u^{0}_{ij}$|')
 
 # Plotting noslit.
 plt.plot(t, abs(1 - noslit))
@@ -17,7 +17,7 @@ plt.show()
 
 # Plotting doubleslit.
 plt.xlabel('t')
-plt.ylabel('1 - $\sum_{i,j}u^{0*}_{ij}u^{0}_{ij}$')
+plt.ylabel('|1 - $\sum_{i,j}u^{0*}_{ij}u^{0}_{ij}$|')
 
 plt.plot(t, abs(1 - doslit))
 plt.savefig("plotting/figures/doubleslit_probabilities.pdf")
